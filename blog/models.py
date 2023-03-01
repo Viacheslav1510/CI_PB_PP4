97f7e223748c4c7a1c89016f79c2945a492f1ab2
slava_kyiv <vyacheslavkondriyanenko@gmail.com>
2023-03-01 20:43:35 +0000

    Create superuser, model for post, and view

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+class PostModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    rating = models.IntegerField(default=0,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(0),
+        ]
+    )
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from .models import PostModel
 # Create your views here.
 
 
 def blog_home(request):
-    return render(request, 'blog/blog.html')
+    posts = PostModel.objects.all()
+    return render(request, 'blog/blog.html', {'posts': posts})
