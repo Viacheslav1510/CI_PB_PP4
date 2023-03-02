922c8af3aa47c5c11f7edaac8a2280dba62e08fe
slava_kyiv <vyacheslavkondriyanenko@gmail.com>
2023-03-02 15:15:13 +0000

    create form for user sign up, create view for sign up

---
+++ accounts/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        for field in ['username', 'email', 'password1', 'password2']:
+            self.fields[field].help_text = None
